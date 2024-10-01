c8cfd7dcea658f34326c92bea5fdeec149cecb23
Beatrycze Volk <beatrycze.volk@slub-dresden.de>
2024-10-01 16:26:49 +0200

    Upgrade versions for release 4.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,19 +3,19 @@ $EM_CONF[$_EXTKEY] = [
     'title' => 'SLUB DiGA.Sax FE Management',
     'description' => 'TYPO3 extension for the DiGA.Sax frontend user management.',
     'category' => 'templates',
-    'author' => 'Stephan Gonder, Felix Franz, Alexander Bigga',
+    'author' => 'Stephan Gonder, Felix Franz, Alexander Bigga, Beatrycze Volk',
     'author_email' => 'typo3@slub-dresden.de',
     'state' => 'beta',
     'internal' => '',
     'uploadfolder' => '0',
     'createDirs' => '',
     'clearCacheOnLoad' => 0,
-    'version' => '3.1.1',
+    'version' => '4.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '10.4.0-10.4.99',
-            'femanager' => '6.3.0-6.3.99',
-            'fe_change_pwd' => '2.0.0-2.99.99'
+            'typo3' => '10.4.0-11.5.99',
+            'femanager' => '7.2.3',
+            'fe_change_pwd' => '3.1.0-3.99.99'
         ],
         'conflicts' => [
         ],
